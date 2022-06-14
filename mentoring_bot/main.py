f7fa99d9b7ee6d8b76256dd6b3fa2773984c338d
Rasul Taimaskhanov <81189712+taimaskhanov11@users.noreply.github.com>
2022-06-14 15:31:47 +0300

    add evaluation message

--- mentoring_bot/main.py
+++ mentoring_bot/main.py
@@ -29,7 +29,7 @@ async def start():
         level="TRACE",
         # old_level=logging.DEBUG,
         old_level=logging.INFO,
-        steaming=True,
+        steaming=False,
         write=True,
     )
 
